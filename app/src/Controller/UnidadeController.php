13486ab71e3b3b2f3c9a4e05bf61b2b980e8bcda
Diego Bastos <d.bastos@marinha.mil.br>
2024-01-25 08:59:26 -0300

    controler unidade

---
+++ app/src/Controller/UnidadeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UnidadeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/unidade')]
+class UnidadeController extends AbstractController
+{
+    public function __construct(private UnidadeRepository $unidadeRepository){}
+
+    #[Route('/', name: 'app_unidade')]
+    public function index(): JsonResponse
+    {
+        return $this->json($this->unidadeRepository->findAll());
+    }
+}
