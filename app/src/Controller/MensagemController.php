e45910ba275d4246b55b69124493637c2f3edd5f
Diego Bastos <d.bastos@marinha.mil.br>
2024-03-25 13:46:34 -0300

    Correção na visualização de mensagens sem nenhum usuário no tramite_futuro

--- app/src/Controller/MensagemController.php
+++ app/src/Controller/MensagemController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Usuario;
-use App\UseCase\Mensagem\ListarMensagens;
 use App\UseCase\Mensagem\BuscarMensagemPeloId;
 use App\UseCase\Mensagem\CadastrarMensagem;
 use App\UseCase\Mensagem\AlterarMensagem;
